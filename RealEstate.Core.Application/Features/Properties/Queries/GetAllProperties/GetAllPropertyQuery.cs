08af7be3f7ac40ac911f55d1fdc6a7fa3b9bb736
Dav <davmontmontas@gmail.com>
2022-08-13 17:27:13 -0400

    add jwt packages to identity

--- RealEstate.Core.Application/Features/Properties/Queries/GetAllProperties/GetAllPropertyQuery.cs
+++ RealEstate.Core.Application/Features/Properties/Queries/GetAllProperties/GetAllPropertyQuery.cs
@@ -1,19 +1,33 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using RealEstate.Core.Application.Interfaces.Repositories;
 using RealEstate.Core.Application.ViewModels.Property;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RealEstate.Core.Application.Features.Properties.Queries.GetAllProperties
 {
+
     public class GetAllPropertyQuery : IRequest<IList<PropertyViewModel>>
     {
         public int? Id { get; set; }
     }
+
+    //Handler Class
     public class GetAllPropertyQueryHandler : IRequestHandler<GetAllPropertyQuery, IList<PropertyViewModel>> 
     {
-        private readonly IPropertyRepository repo;
+        private readonly IPropertyRepository _repo;
+        private readonly IMapper _mapper;
+        public GetAllPropertyQueryHandler(IPropertyRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<PropertyViewModel>> Handle(GetAllPropertyQuery request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
     }
 }
