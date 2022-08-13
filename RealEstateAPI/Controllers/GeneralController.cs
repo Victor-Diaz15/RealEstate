bb7d97bb03d7833832e1ff28a2e098b38cf926c6
Dav <davmontmontas@gmail.com>
2022-08-13 02:28:01 -0400

    add general controller

---
+++ RealEstateAPI/Controllers/GeneralController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateAPI.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class GeneralController : ControllerBase
+    {
+    }
+}
---
+++ RealEstateAPI/Controllers/v1/PropertiesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateAPI.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class PropertiesController : GeneralController
+    {
+
+    }
+}
