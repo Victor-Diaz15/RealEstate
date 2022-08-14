020db49d94b337df2e78a39bf5263e23ad1e52fa
Dav <davmontmontas@gmail.com>
2022-08-13 22:59:39 -0400

    add class that automatically generate property codes

---
+++ RealEstate.Core.Application/Helpers/PropertyCodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Core.Application.Helpers
+{
+    public class PropertyCodeGenerator
+    {
+        public string PropertyCodeGen()
+        {
+            Random randomNumber = new Random();
+            int number = randomNumber.Next(1, 10000000);
+            string generatedCode = number.ToString("0000000");
+            return generatedCode;
+        }
+    }
+}
