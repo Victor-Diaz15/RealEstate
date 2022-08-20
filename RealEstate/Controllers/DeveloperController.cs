da76e162f65dbfc90753667148344600bcaa3f01
Ronald Brito <britoskies@gmail.com>
2022-08-20 19:40:38 -0400

    Add developer module and fix admin views

--- RealEstate/Controllers/DeveloperController.cs
+++ RealEstate/Controllers/DeveloperController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Core.Application.Dtos.Account;
 using RealEstate.Core.Application.Enums;
+using RealEstate.Core.Application.Helpers;
 using RealEstate.Core.Application.Interfaces.Services;
 using RealEstate.Core.Application.ViewModels.User;
 using System.Collections.Generic;
@@ -11,9 +15,13 @@ namespace WebApp.RealEstate.Controllers
     public class DeveloperController : Controller
     {
         private readonly IUserService _userService;
-        public DeveloperController(IUserService userService)
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public DeveloperController(IUserService userService, IMapper mapper, IHttpContextAccessor httpContextAccessor)
         {
             _userService = userService;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<IActionResult> Index()
@@ -24,16 +32,64 @@ namespace WebApp.RealEstate.Controllers
             return View(users);
         }
 
-        public IActionResult RegisterDev() 
+        public IActionResult Add()
         {
-            return View(new ManagerSaveViewModel());
+            return View("Save", new ManagerSaveViewModel());
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> RegisterDev(ManagerSaveViewModel vm)
-        //{
+        [HttpPost]
+        public async Task<IActionResult> Add(ManagerSaveViewModel vm)
+        {
+            if (!ModelState.IsValid) return View("Save", vm);
 
-        //    return View();
-        //}
+            RegisterManagerResponse response = await _userService.RegisterDevAsync(vm);
+            if (response.HasError)
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View("Save", vm);
+            }
+
+            return RedirectToRoute(new { controller = "Developer", action = "Index" });
+
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            var user = HttpContext.Session.Get<AuthenticationResponse>("user");
+
+            if (id == user.Id)
+            {
+                return RedirectToRoute(new { controller = "Developer", action = "Index" });
+            }
+            UserSaveViewModel vm = await _userService.GetUserByIdAsync(id);
+            ManagerSaveViewModel managerSaveVm = _mapper.Map<ManagerSaveViewModel>(vm);
+            return View("Save", managerSaveVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ManagerSaveViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Save", vm);
+            }
+            UserSaveViewModel userSaveVm = _mapper.Map<UserSaveViewModel>(vm);
+            await _userService.UpdateUserAsync(userSaveVm, userSaveVm.Id);
+
+            return RedirectToRoute(new { controller = "Developer", action = "Index" });
+        }
+
+        public async Task<IActionResult> ActiveUser(string id)
+        {
+            return View("ActiveUser", await _userService.GetUserByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ActiveUser(UserSaveViewModel vm)
+        {
+            await _userService.ActivedUserAsync(vm.Id);
+            return RedirectToRoute(new { controller = "Developer", action = "Index" });
+        }
     }
 }
